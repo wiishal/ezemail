f4219a798065acd7d46829ba5e6b9280202e4d42
vishal-Nanaware <vishalnanaware7850@gmail.com>
2025-01-25 00:34:23 +0530

    modify:ui changes

--- src/app/generate/page.tsx
+++ src/app/generate/page.tsx
@@ -1,10 +1,23 @@
 import { JSX } from "react";
-import Generate from "../component/generate";
+import { Metadata } from "next";
+import GeneratePage from "../pages/GeneratePage";
 
 export default function page(): JSX.Element {
   return (
-    <div>
-      <Generate />
+    <div
+      className={`flex flex-col align-middle bg-neutral-900 justify-center items-center w-full h-screen`}
+    >
+      <div className="font-luckyguy text-3xl text-pink-400  max-h-fit border-red-600">
+        EZ EMAIL
+      </div>
+      <div className="h-5/6 w-full ">
+
+      <GeneratePage />
+      </div>
     </div>
   );
 }
+export const metadata: Metadata = {
+  title: "Generate With EZ",
+  description: "Generate Email/learn with Proffetional tone",
+};
--- src/app/learn/page.tsx
+++ src/app/learn/page.tsx
@@ -1,9 +1,20 @@
-"use client";
-import { useUser } from "@auth0/nextjs-auth0/client";
-export default function learn() {
-  const { user } = useUser();
+import { JSX } from "react";
+import { Metadata } from "next";
+import LearnPage from "../pages/LearnPage";
 
-  if (!user) return <div>notlogged</div>;
-
-  if (user) return <div>{user.name}</div>;
+export default function page(): JSX.Element {
+  return (
+    <div
+      className={`flex flex-col align-middle bg-neutral-900 justify-center items-center w-full h-screen`}
+    >
+      <div className="font-luckyguy text-3xl text-pink-400 border border-black m-5">
+        EZ EMAIL
+      </div>
+      <LearnPage />
+    </div>
+  );
 }
+export const metadata: Metadata = {
+  title: "Learn With EZ",
+  description: "Generate Email/learn with Proffetional tone",
+};
