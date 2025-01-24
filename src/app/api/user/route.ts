87b200c723c92fc7dd89a7f899530df30f754c3e
vishal-Nanaware <vishalnanaware7850@gmail.com>
2025-01-24 14:00:19 +0530

    added: User endpoint

---
+++ src/app/api/user/route.ts
@@ -0,0 +1,22 @@
+import { getGroqChatCompletion } from "@/app/services/groq";
+import { NextRequest, NextResponse } from "next/server";
+
+export async function POST(req: NextRequest) {
+  const body = await req.json();
+  try {
+    console.log("started");
+    const chatCompletion = await getGroqChatCompletion(body.prompt);
+    console.log( "chat end");
+    if (chatCompletion) {
+      return NextResponse.json(
+        { message: chatCompletion.choices[0]?.message?.content },
+        { status: 200 }
+      );
+    }
+  } catch (error) {
+    return NextResponse.json({
+      error: "failed during processing request",
+      status: 400,
+    });
+  }
+}
